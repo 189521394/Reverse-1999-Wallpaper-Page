6ca0965eec45b181f0a5f8d42221b221e6724478
189521394 <Win___Lx@163.com>
2026-01-12 00:19:18 +0800

    少了个文件

---
+++ js/search/itemListener/addTagFromPic.js
@@ -0,0 +1,13 @@
+document.getElementById("returnTag").addEventListener("click",function (e) {
+    let target = document.getElementById("submitPool");
+    let tag = e.target.closest(".tags");
+
+    // 防止点击空白处报错
+    if (!tag) return;
+
+    let newTag = tag.cloneNode(true);
+    // 替换class，适配新的父元素
+    newTag.classList.replace("tags", "waiting");
+
+    target.appendChild(newTag);
+});
