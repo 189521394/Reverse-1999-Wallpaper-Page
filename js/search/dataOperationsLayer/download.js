ba54eb8259c0b3d6472e0d65afe03e1d1bbfe707
189521394 <2221771663@qq.com>
2026-02-27 22:20:22 +0800

    添加跨域请求和缓存下载功能，重新测试

--- js/search/dataOperationsLayer/download.js
+++ js/search/dataOperationsLayer/download.js
@@ -10,9 +10,12 @@ async function download() {
 
     // 尝试下载
     try {
+        // 此为测试
         // 添加时间戳，让浏览器强制绕过缓存，从服务器申请图片
-        const bypassCacheUrl = imgURL + (imgURL.includes('?') ? '&' : '?') + '_t=' + new Date().getTime();
-        const response = await fetch(bypassCacheUrl);
+        // const bypassCacheUrl = imgURL + (imgURL.includes('?') ? '&' : '?') + '_t=' + new Date().getTime();
+        // const response = await fetch(bypassCacheUrl);
+        // 走正常路径下载
+        const response = await fetch(imgURL);
         const blob = await response.blob();
 
         // 内存中生成专属链接并点击下载
