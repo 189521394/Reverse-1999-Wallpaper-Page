8c831f44422ba713f65a077c0554c020de1d989f
189521394 <2221771663@qq.com>
2026-04-13 23:25:15 +0800

    更新多语言系统

--- js/index/guide.js
+++ js/index/guide.js
@@ -1,1 +1,29 @@
-const guide = document.getElementById('guide');
+const guidePage = document.getElementById('guide');
+
+const optionZH = document.getElementById('optionZH');
+const optionEN = document.getElementById('optionEN');
+
+const choiceZHApper = document.getElementById('choiceZH');
+const choiceENApper = document.getElementById("choiceEN");
+
+// guidePage.classList.add("show");
+
+// const lockID = "guide_" + (++lockCounter)
+// requestScrollLock(lockID);
+
+// 统一刷新按钮
+function refreshRadioOption() {
+    if (optionZH.checked) {
+        choiceZHApper.classList.add("checked");
+        choiceENApper.classList.remove("checked");
+    } else {
+        choiceENApper.classList.add("checked");
+        choiceZHApper.classList.remove("checked");
+    }
+}
+
+// 点击刷新
+optionZH.addEventListener('click', refreshRadioOption);
+optionEN.addEventListener('click', refreshRadioOption);
+
+document.addEventListener('DOMContentLoaded', refreshRadioOption);
