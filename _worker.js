79c68f159b6f0dc8344aad72d84cc3df5c9e8c51
189521394 <2221771663@qq.com>
2026-04-29 23:05:09 +0800

    SEO优化

--- _worker.js
+++ _worker.js
@@ -17,21 +17,43 @@ export default {
                         element.setAttribute("lang", "en");
                     }
                 })
+                // 重写网页标题
+                .on('title', {
+                    element(element) {
+                        element.setInnerContent("Reverse: 1999 Wallpapers | Official CG, Mobile & Desktop");
+                    }
+                })
+                // 重写分享标题
+                .on('meta[property="og:title"]', {
+                    element(element) {
+                        element.setAttribute("content", "Reverse: 1999 Wallpapers | Official CG, Mobile & Desktop");
+                    }
+                })
+                // 重写描述
                 .on('meta[name="description"]', {
                     element(element) {
-                        element.setAttribute("content", "High-quality wallpaper gallery dedicated to Reverse: 1999. Featuring a massive collection of official CG images, ready for quick filtering and download.");
+                        element.setAttribute("content", "An HD wallpaper library designed exclusively for Reverse: 1999. It features a massive collection of official CG artwork, available for quick filtering and download.");
                     }
                 })
+                // 重写关键词
                 .on('meta[name="keywords"]', {
                     element(element) {
-                        element.setAttribute("content", "Reverse 1999, Reverse 1999 wallpapers, Reverse 1999 CG, game wallpapers, anime wallpapers");
+                        element.setAttribute("content", "Reverse 1999, Reverse: 1999 wallpapers, Reverse 1999 official CG, HD wallpapers, desktop wallpapers, mobile wallpapers, anime game wallpapers");
                     }
                 })
+                // 重写og描述
                 .on('meta[property="og:description"]', {
                     element(element) {
-                        element.setAttribute("content", "High-quality wallpaper gallery dedicated to Reverse: 1999. Featuring a massive collection of official CG images, ready for quick filtering and download.");
+                        element.setAttribute("content", "An HD wallpaper library designed exclusively for Reverse: 1999. It features a massive collection of official CG artwork, available for quick filtering and download.");
                     }
                 })
+                // 替换H1标题
+                .on('h1#h1-title', {
+                    element(element) {
+                        element.setInnerContent("Reverse: 1999 Wallpapers | Official CG, Mobile & Desktop");
+                    }
+                })
+                // ogURL
                 .on('meta[property="og:url"]', {
                     element(element) {
                         // 动态保留所有高级搜索参数
